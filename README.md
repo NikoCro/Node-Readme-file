07eb7e5bbd41b0c1fba1c8d697319f686baa53b6
Nikola Lucic <nikola1983@yahoo.com>
2023-02-08 14:16:42 -0500

    created readme file

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Node-Readme-file
+
+  # PROJECT TITLE: bla
+
+  ## DESCRIPTION
+
+    bla
+
+  ##TABLE OF CONTENTS
+
+  1. [INSTALLATION](#installation)
+  2. [USAGE](#usage)
+  3. [LICENSE](#license)
+  4. [CONTRIBUTION](#contibution)
+  5. [TESTS](#tests)
+  6. [QUESTIONS](#questions)
+
+  ##INSTALATION
+
+  none
+
+  ##USAGE
+
+  nothing
+
+  ##LICENSE
+
+  gpl
+
+  ##CONTRIBUTION
+
+  nothing
+
+  ##TESTS
+
+  test run
+
+  ##QUESTIONS
+
+  dfnsjfjs
+
+  ldnsln
+
+  
---
+++ index.js
@@ -0,0 +1,114 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateReadme = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contribution,
+  tests,
+  email,
+  github,
+}) =>
+  `
+  # PROJECT TITLE: ${title}
+
+  ## DESCRIPTION
+
+    ${description}
+
+  ##TABLE OF CONTENTS
+
+  1. [INSTALLATION](#installation)
+  2. [USAGE](#usage)
+  3. [LICENSE](#license)
+  4. [CONTRIBUTION](#contibution)
+  5. [TESTS](#tests)
+  6. [QUESTIONS](#questions)
+
+  ##INSTALATION
+
+  ${installation}
+
+  ##USAGE
+
+  ${usage}
+
+  ##LICENSE
+
+  ${license}
+
+  ##CONTRIBUTION
+
+  ${contribution}
+
+  ##TESTS
+
+  ${tests}
+
+  ##QUESTIONS
+
+  ${email}
+
+  ${github}
+
+  `;
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What is your project about?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "what do you need to know about using the repo?",
+    },
+    {
+      type: "list",
+      name: "license",
+      choices: ["mit", "gpl", "apache", "none"],
+      message: "What kind of license should your project have?",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "What does the user need to know abut contributing to the repo?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command should be run to run tests?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your Github page?",
+    },
+  ])
+  .then((answers) => {
+    const readmeContents = generateReadme(answers);
+
+    fs.writeFile("README.md", readmeContents, (err) =>
+      err ? console.log(err) : console.log("Successfully created Readme.md!")
+    );
+  });
